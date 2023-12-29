75f1104fda2257e2267080a74053f1fd27fa6ccb
Miqueias Teofani <projetosmiqueias@gmail.com>
2023-12-29 19:49:29 +0000

    Comment debug

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def index():
     return render_template('index.html', listMatches = ListMatches)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
